d3b0a55ddba86648810524116651e2cec28e04d7
dimaa <dimanechepurenko42@gmail.com>
2026-09-16 22:23:48 +0600

    lesson 12

--- oop.py
+++ oop.py
@@ -1,1 +1,16 @@
-print ("hello")
+from abc import ABC, abstractmethod
+
+class Car(ABC):
+    @abstractmethod
+    def test(self):
+        pass
+
+class TestCar(Car):
+    def car(self):
+        print("car")
+
+    def test(self):
+        print('test')
+
+car = TestCar()
+print(car.car())
